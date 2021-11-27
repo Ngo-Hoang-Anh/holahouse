06ba6d4af3945947f31ad3ddba5446ba6edc9c35
Ngo-Hoang-Anh <ngohoanganh2922000@gmail.com>
2021-11-27 19:21:22 +0700

    http

--- src/common/common.js
+++ src/common/common.js
@@ -1,2 +1,2 @@
 export const REACT_APP_API_BASE_URL =
-  "https://ec2-54-169-33-217.ap-southeast-1.compute.amazonaws.com:8080/api";
+  "http://ec2-54-169-33-217.ap-southeast-1.compute.amazonaws.com:8080/api";
